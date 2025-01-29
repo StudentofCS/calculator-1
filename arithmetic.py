ddb61fcb10ccc90d26537b293e3af625983efbcb
Luis Minaya <luis.minaya@gmail.com>
2025-01-28 21:48:07 -0500

    Added square, cube, power, mod

--- arithmetic.py
+++ arithmetic.py
@@ -23,15 +23,19 @@ def divide(num1, num2):
 
 def square(num1):
     """Return the square of num1."""
+    return num1**2
 
 
 def cube(num1):
     """Return the cube of num1."""
+    return num1 ** 3
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
+    return num1 ** num2
 
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    return num1 % num2
